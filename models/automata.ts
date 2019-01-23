70141d5cf7ae73b818eb9d362fd1b57089901fd1
Damaso Fernandez <damasofc@hotmail.com>
2019-01-23 13:35:42 -0600

    clases modelos listas

--- models/automata.ts
+++ models/automata.ts
@@ -1,6 +1,18 @@
+import Estado from './estado';
+import Transicion from './transicion';
 export default class Automata {
+    estados:Array<Estado>;
+    transiciones:Array<Transicion>;
     constructor(params:any) {
-        
+        this.estados = new Array();
+        this.transiciones = new Array();
+    }
+
+    getInitialState() {
+        this.estados.forEach(element => {
+            if(element.esInicial)
+                return element;
+        });
     }
 };
 
