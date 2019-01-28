4d905bf8ee14ef3c8b5f75a83e5d6b2a1fd836e7
Damaso Fernandez <damasofc@hotmail.com>
2019-01-28 11:31:15 -0600

    Revert "lista la clase Automata y la funcion evaluar"
    This reverts commit 91595ccfc85e8162821444354ff1dffc0c985c79.

--- models/automata.ts
+++ models/automata.ts
@@ -5,21 +5,19 @@ export default class Automata {
         this.estados = new Map();
     }
 
-    getInitialState() {
-        var estado:any = null;
-        this.estados.forEach((elem:Estado, key:Number)  => {
-            if(elem.esInicial === true){
-                estado = elem;
-            }
+    getInitialState():Estado {
+        this.estados.forEach(element => {
+            if(element.esInicial)
+                return element;
         });
-        return estado;
+        return new Estado("null");
     }
 
     evaluar(palabra: String): boolean{
         var x = 0;
         var state:any = this.getInitialState();
         var char = palabra.charAt(x);
-        while (char.length > 0) {
+        while (char != null) {
             state = this.getNextState(state,char);
             if(state == null)
             {
@@ -35,9 +33,9 @@ export default class Automata {
     }
 
     getNextState(actual:Estado,char:String){
-        var trans = actual.transiciones.get(char);
-        if(trans!= null){
-            return trans.stEnd;
+        var nxState = actual.transiciones.get(char);
+        if(nxState != null){
+            return nxState;
         }
         return null;
     }
